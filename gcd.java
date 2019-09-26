67449a7a24cdbe73c61919cfa724f8349eb7ffaf
KALYANKUMAR13 <35223898+KALYANKUMAR13@users.noreply.github.com>
2019-09-26 22:32:51 +0530

    Create gcd.java

---
+++ gcd.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n1 = scan.nextInt();
+		int n2 = scan.nextInt();
+	 
+	    while(n1!=n2)
+		if(n1<n2)
+		 {
+		   n1 = n1-n2;  
+		 }
+		 else
+		 {
+		     n2 = n2-n1;
+		 }
+		 System.out.print("The GCD of given numbers is:"n1);
+	}
+}
