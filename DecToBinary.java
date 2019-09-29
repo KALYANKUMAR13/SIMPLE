12f6cd290dcd2889a1ea5a8302b917787802fd99
KALYANKUMAR13 <35223898+KALYANKUMAR13@users.noreply.github.com>
2019-09-29 17:38:37 +0530

    Create DecToBinary.java

---
+++ DecToBinary.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int[] bin = new int[1100];
+		int c=0;
+		while(n>0)
+		{
+		    bin[c] = n%2;
+		    n = n/2;
+		    c++;
+		}
+		for(int i=0;i<c;i++)
+		{
+		    System.out.print(bin[i]);
+		}
+	}
+}
