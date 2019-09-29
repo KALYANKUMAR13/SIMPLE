986b0dacf227727bc27205c7f31282c5890fa816
KALYANKUMAR13 <35223898+KALYANKUMAR13@users.noreply.github.com>
2019-09-29 23:30:42 +0530

    Update missingNum.java

--- missingNum.java
+++ missingNum.java
@@ -7,8 +7,10 @@ public class Main
 		int ar[] = new int[n];
 		for(int i=0;i<n;i++)
 		{
-		    ar[i] = scan.nextInt();
+		    ar[i] = scan.nextInt();   
 		}
+		//Add the array elements  2.int y = n(n+1/2);
+		//Subract int r = y-arel;//
 		for(int i=0;i<n-1;i++)
 		{
 		    if(ar[i]+1!=ar[i+1])
