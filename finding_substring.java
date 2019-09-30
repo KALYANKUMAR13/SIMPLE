9972c2c76b6e6f629b2e3136aa7fa5fb3dd75817
KALYANKUMAR13 <35223898+KALYANKUMAR13@users.noreply.github.com>
2019-09-30 05:52:25 +0530

    Update finding_substring.java

--- finding_substring.java
+++ finding_substring.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 public class Main
 {
@@ -10,7 +9,7 @@ public class Main
 		int c=0;
 		for(int i=0;i<s.length()-fin.length()+1;i++)
 		{
-		    for( j=i; j<fin.length()+i; j++)
+		    for(int  j=i; j<fin.length()+i; j++)
 		    {
 		        p=p+s.charAt(j);
 		    }
@@ -22,10 +21,7 @@ public class Main
 		    }
 		    p="";
 		}
-		for(int i=0;i<y;i++)
-		{
-		    System.out.print(arst[i]+" ");
-		}
+		System.out.print(fin);
 		System.out.print(c);
 		
 	}
