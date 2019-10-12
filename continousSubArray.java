9e264d97cdafeb9379b980730e7e1c05797cdd14
KALYANKUMAR13 <35223898+KALYANKUMAR13@users.noreply.github.com>
2019-10-12 22:31:28 +0530

    Create continousSubArray.java

---
+++ continousSubArray.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int fin = scan.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i]=scan.nextInt();
+		}
+		 int temar[] = new int[1000];
+		 int c=0,sum=0;
+		 for(int i=0;i<n;i++)
+		 {
+		     sum=0;
+		     c=0;
+		     for(int j=i; j<n; j++)
+		     {
+		         sum = sum+arr[j];
+		         temar[c] = arr[j];
+		         c++;
+		         if(sum == fin)
+		         {
+		             for(int k=0;k<c;k++)
+		             {
+		                 System.out.print(temar[k]+" ");
+		             }
+		             break;
+		         }
+
+
+		     }
+		    for(int k=0;k<c;k++)
+		      temar[k]=0;
+		 }
+	}
+}
