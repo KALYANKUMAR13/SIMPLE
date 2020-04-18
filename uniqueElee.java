c3f3346fb6bc42ae1b9efb055c0b229984a1cc97
KALYANKUMAR13 <35223898+KALYANKUMAR13@users.noreply.github.com>
2020-04-18 19:02:50 +0530

    Create uniqueElee.java

---
+++ uniqueElee.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i] = scan.nextInt();
+		}
+		int ne[] = new int[n];
+		int c=0;
+		boolean now = false;
+		for(int i=0;i<n;i++)
+		{
+		    
+		    for(int j=0;j<n;j++)
+		    {
+		       if(arr[i]== ne[j])
+		       {
+		           now = false;
+		           break;
+		       }
+		       else
+		       {
+		          now = true;
+		       }
+		   }
+		   if(now)
+		   {
+		       ne[c]= arr[i];
+		       c++;
+		   }
+		   
+		   
+		     
+		}
+		for(int i=0;i<c;i++)
+		{
+		    System.out.print(ne[i]+" ");
+		}
+	}
+}
